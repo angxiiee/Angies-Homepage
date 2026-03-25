272c41e0115a1813a711e46489d1dfb5a6e63be7
angxiiee <ariasa2@vcu.edu>
2026-03-24 22:08:30 -0400

    I FINISHED

---
+++ Control/main.js
@@ -0,0 +1,24 @@
+$(".button").click(function(){
+    const color = $(this).data("color");
+    
+    const newBlock = $("<div></div>")
+        .addClass("block")
+        .css("background-color", color);
+
+    $(".container").append(newBlock);
+
+    newBlock.draggable({
+        containment: ".container",
+
+        start: function() {
+            $(this).css("cursor", "grabbing");
+        },
+        stop: function() {
+            $(this).css("cursor", "grab");
+        }
+    });
+});
+
+$(".reset-btn").click(function() {
+    $(".block").remove();
+});
